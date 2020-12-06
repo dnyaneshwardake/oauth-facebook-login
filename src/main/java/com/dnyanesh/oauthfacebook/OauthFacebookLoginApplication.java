0be9c0805a20ac2ed4c5f0e83bcaf83e88d42478
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2020-12-06 09:25:19 +0530

    Add files via upload

---
+++ src/main/java/com/dnyanesh/oauthfacebook/OauthFacebookLoginApplication.java
@@ -0,0 +1,13 @@
+package com.dnyanesh.oauthfacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthFacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthFacebookLoginApplication.class, args);
+	}
+
+}
